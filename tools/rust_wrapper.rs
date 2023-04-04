eba4d88c2b206a515cfa7e635d6d8b2e026fd53b
Chanjung Kim <freiyer.paxbun@gmail.com>
2023-04-04 12:49:35 +0900

    Consider rust_wrapper.rs as a header

--- tools/rust_wrapper.rs
+++ tools/rust_wrapper.rs
@@ -3,6 +3,9 @@
 // Author:      Chanjung Kim
 // Created:     29/03/2023
 // Copyright (c) Authors and others. All rights reserved.
+//
+// This wrapper is considered one of the library header files mentioned in
+// the GNU Lesser General Public License document.
 /////////////////////////////////////////////////////////////////////////////
 
 use autocxx::prelude::*;
